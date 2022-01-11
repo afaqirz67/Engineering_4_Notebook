f3fa8243798c25b58876d7d8cbb8fabf54e0811c
afaqirz67 <56890879+afaqirz67@users.noreply.github.com>
2022-01-11 09:49:22 -0500

    Update simpletest.py

--- Adafruit_Python_LSM303/examples/simpletest.py
+++ Adafruit_Python_LSM303/examples/simpletest.py
@@ -1,21 +1,65 @@
-# Simple demo of of the LSM303 accelerometer & magnetometer library.
-# Will print the accelerometer & magnetometer X, Y, Z axis values every half
-# second.
-# Author: Tony DiCola
-# License: Public Domain
 import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
 
 # Import the LSM303 module.
 import Adafruit_LSM303
 
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 # Create a LSM303 instance.
 lsm303 = Adafruit_LSM303.LSM303()
 
+
+# Raspberry Pi pin configuration:
+RST = 24
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+
+# Note you can change the I2C address by passing an i2c_address parameter like:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
+
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = 2
+shape_width = 20
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = padding
+
+
+
+
 # Alternatively you can specify the I2C bus with a bus parameter:
 #lsm303 = Adafruit_LSM303.LSM303(busum=2)
 
-print('Printing accelerometer & magnetometer X, Y, Z axis values, press Ctrl-C to quit...')
 while True:
     # Read the X, Y, Z axis acceleration values and print them.
     accel, mag = lsm303.read()
@@ -26,3 +70,20 @@ while True:
           accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
     # Wait half a second and repeat.
     time.sleep(0.5)
+
+
+
+
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+
+# Write two lines of text.
+//draw.text((x, top),    'Hello',  font=font, fill=255)
+//draw.text((x, top+20), 'Henry!', font=font, fill=255)
+ 	
+# Display image.
+disp.image(image)
+disp.display()

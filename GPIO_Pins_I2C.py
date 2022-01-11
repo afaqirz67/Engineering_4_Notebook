35e25d5057633eacb0f5b0078b2977ceb4c6d3a5
afaqirz67 <afaqirz67@charlottesvilleschools.org>
2022-01-11 15:44:50 +0000

    updated GPIO

--- GPIO_Pins_I2C.py
+++ GPIO_Pins_I2C.py
@@ -27,13 +27,13 @@ SPI_DEVICE = 0
 # SPI_DEVICE = 0
 
 # 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+#disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 # 128x64 display with hardware I2C:
 # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
 # Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
 
 # Alternatively you can specify an explicit I2C bus number, for example
 # with the 128x32 display you would use:
@@ -88,7 +88,7 @@ while True:
     # Grab the X, Y, Z components from the reading and print them out.
     accel_x, accel_y, accel_z = accel
     mag_x, mag_y, mag_z = mag
-    drawtext(accel_x)
+    draw.text(accel_x)
     # Display image.
     disp.image(image)
     disp.display()

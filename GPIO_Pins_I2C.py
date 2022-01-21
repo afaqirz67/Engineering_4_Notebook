9ca28b031279eabe99d9a14a83cede96ad393d01
afaqirz67 <56890879+afaqirz67@users.noreply.github.com>
2022-01-21 13:29:04 -0500

    Update GPIO_Pins_I2C.py

--- GPIO_Pins_I2C.py
+++ GPIO_Pins_I2C.py
@@ -6,45 +6,61 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+
+
+
+# Raspberry Pi pin configuration:
 RST = 24
+# Note the following are only used with SPI:
 DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
+
 lsm303 = Adafruit_LSM303.LSM303()
-# Alternatively you can specify the I2C bus with a bus parameter:
-#lsm303 = Adafruit_LSM303.LSM303(busum=2)
-# Read the X, Y, Z axis acceleration values and print them.
-accel, mag = lsm303.read()
-# Grab the X, Y, Z components from the reading and print them out.
-
 mag_x, mag_y, mag_z = mag
-
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
+
+
+# Initialize library.
 disp.begin()
+# Clear display.
 disp.clear()
 disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 
+# Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-font = ImageFont.load_default()
-top = disp.height
-while True:
-   accel, mag = lsm303.read()
-   accel_x, accel_y, accel_z = accel
-   draw.rectangle((0,0,width,height), outline=0, fill=0)
-   draw.text((10, top/1.5),f"x: {round(accel_x/109.69, 2)}",  font=font, fill=55)
-   draw.text((50, top/3),f"y: {round(accel_y/109.69, 2)}", font=font, fill=55)
-   draw.text((80, 2),f"z: {round(accel_z/109.69, 2)}", font=font, fill=55)
-   disp.image(image)
-   disp.display()
-   
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+shape_width = 20
+bottom = height-padding
 
-# draw.text((x, top),    'Hello',  font=font, fill=255)
-# draw.text((x, top+20), 'World!', font=font, fill=255)
+# Load default font.
+font = ImageFont.load_default()
+
+while True:
+    accel, mag = lsm303.read()
+    accel_x, accel_y, accel_z = accel
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    draw.text((5, 2), f" X: {round(accel_x/102.34)} ",  font=font, fill=55)
+    draw.text((5, 22), f" Y: {round(accel_y/102.34)}", font=font, fill=55)
+    draw.text((5, 22), f" Z: {round(accel_z/102.34)}", font=font, fill=55)
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    # Display image.
+    disp.image(image)
+    disp.display()
+    # Wait half a second and repeat.
+    time.sleep(1/4)
+
 
 disp.image(image)
 disp.display()

41411400aaff24393fe2a379d114a0518af8e7c4
afaqirz67 <56890879+afaqirz67@users.noreply.github.com>
2022-01-21 11:59:44 -0500

    Update GPIO_Pins_I2C.py

--- GPIO_Pins_I2C.py
+++ GPIO_Pins_I2C.py
@@ -1,121 +1,50 @@
 import time
-
 import Adafruit_LSM303
-
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
-
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-lsm303 = Adafruit_LSM303.LSM303()
-
-
-# Raspberry Pi pin configuration:
 RST = 24
-# Note the following are only used with SPI:
 DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
+lsm303 = Adafruit_LSM303.LSM303()
+# Alternatively you can specify the I2C bus with a bus parameter:
+#lsm303 = Adafruit_LSM303.LSM303(busum=2)
+# Read the X, Y, Z axis acceleration values and print them.
+accel, mag = lsm303.read()
+# Grab the X, Y, Z components from the reading and print them out.
 
-# Beaglebone Black pin configuration:
-# RST = 'P9_12'
-# Note the following are only used with SPI:
-# DC = 'P9_15'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
-
-# 128x32 display with hardware I2C:
-#disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-# 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-
-# Note you can change the I2C address by passing an i2c_address parameter like:
+mag_x, mag_y, mag_z = mag
 
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
-
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
-
-
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
-
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
-
-# Initialize library.
 disp.begin()
-
-# Clear display.
 disp.clear()
 disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = 2
-shape_width = 20
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = padding
-
-# Load default font.
 font = ImageFont.load_default()
-
+top = disp.height
 while True:
-  # Read the X, Y, Z axis acceleration values and print them.
-    accel, mag = lsm303.read()
-    # Grab the X, Y, Z components from the reading and print them out.
-    accel_x, accel_y, accel_z = accel
-    mag_x, mag_y, mag_z = mag
+   accel, mag = lsm303.read()
+   accel_x, accel_y, accel_z = accel
+   draw.rectangle((0,0,width,height), outline=0, fill=0)
+   draw.text((10, top/1.5),f"x: {round(accel_x/109.69, 2)}",  font=font, fill=55)
+   draw.text((50, top/3),f"y: {round(accel_y/109.69, 2)}", font=font, fill=55)
+   draw.text((80, 2),f"z: {round(accel_z/109.69, 2)}", font=font, fill=55)
+   disp.image(image)
+   disp.display()
+   
 
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-    draw.text((x, top),   f" X: {round(accel_x/102.34)} ",  font=font, fill=55)
-    draw.text((x, top+20), f" Y: {round(accel_y/102.34)}", font=font, fill=55)
-    draw.text((x, top+40), f" Z: {round(accel_z/102.34)}", font=font, fill=55)
+# draw.text((x, top),    'Hello',  font=font, fill=255)
+# draw.text((x, top+20), 'World!', font=font, fill=255)
 
-
-    draw.text((5,5),   f" accel x {accel_x} ",  font=font, fill=55)
-    draw.text((x, top+20), f"accel_y {accel_z}", font=font, fill=55)
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-    # Display image.
-    disp.image(image)
-    disp.display()
-    # Wait half a second and repeat.
-    time.sleep(000.5)
-
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-#font = ImageFont.truetype('Minecraftia.ttf', 8)
-
-
-# Display image.
 disp.image(image)
 disp.display()
-

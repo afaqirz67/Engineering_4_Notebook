46e1261361139d35a4f8aa3d76efd428519ae1c5
afaqirz67 <56890879+afaqirz67@users.noreply.github.com>
2022-01-27 13:23:10 -0500

    Update copypasta.py

--- copypasta.py
+++ copypasta.py
@@ -1,1 +1,10 @@
-
+from picamera import PiCamera
+import time
+camera = PiCamera()
+camera.resolution = (1280, 720)
+#camera.vflip = True
+#camera.contrast = 10
+#camera.image_effect = "watercolor"
+time.sleep(2)
+camera.capture("/home/pi/Pictures/img.jpg")
+print("Done.")

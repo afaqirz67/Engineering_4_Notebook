78a15e1839e9c77994e876979f7e0e608021b1fa
afaqirz67 <afaqirz67@charlottesvilleschools.org>
2022-01-27 18:42:58 +0000

    upt

--- copypasta.py
+++ copypasta.py
@@ -6,5 +6,13 @@ camera.resolution = (1280, 720)
 #camera.contrast = 10
 #camera.image_effect = "watercolor"
 time.sleep(2)
-camera.capture("/home/pi/Pictures/img.jpg")
-print("Done.")
+camera.start_preview()
+frame = 1
+while True:
+    try:
+	button.wait_for_press()
+	camera.capture("/home/pi/Documents/Engineering_4_Notebook/copypasta.py/img.jpg") 
+        frame += 1
+    except KeyboardInterrupt:
+        camera.stop_preview()
+        break

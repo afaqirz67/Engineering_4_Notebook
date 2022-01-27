433b707916865b188dbac28f6c56ec8e5e8a719a
afaqirz67 <56890879+afaqirz67@users.noreply.github.com>
2022-01-27 13:55:20 -0500

    Update copypasta.py

--- pastafile/copypasta.py
+++ pastafile/copypasta.py
@@ -14,7 +14,7 @@ frame = 1
 while True:
     try:
         button.wait_for_press()
-        camera.capture("/home/pi/Documents/Engineering_4_Notebook/copypasta.py/img.jpg") 
+        camera.capture(f"/home/pi/Documents/Engineering_4_Notebook/pastafile/{frame}.jpg") 
         frame += 1
     except KeyboardInterrupt:
         camera.stop_preview()

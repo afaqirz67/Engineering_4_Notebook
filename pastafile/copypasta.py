9f7d0ad52e37ab16ed38639e2223a86963d0c6a6
afaqirz67 <afaqirz67@charlottesvilleschools.org>
2022-01-27 19:05:39 +0000

    update

--- pastafile/copypasta.py
+++ pastafile/copypasta.py
@@ -11,10 +11,12 @@ camera.resolution = (1280, 720)
 time.sleep(2)
 camera.start_preview()
 frame = 1
+print('code working')
 while True:
     try:
         button.wait_for_press()
         camera.capture(f"/home/pi/Documents/Engineering_4_Notebook/pastafile/{frame}.jpg") 
+        print('working..')
         frame += 1
     except KeyboardInterrupt:
         camera.stop_preview()

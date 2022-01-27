0a704e85679e8e51891cc278e3908493dbb2c998
afaqirz67 <afaqirz67@charlottesvilleschools.org>
2022-01-27 14:57:18 +0000

    code update

--- pics/pics.py
+++ pics/pics.py
@@ -1,11 +1,19 @@
 import time
 import picamera
 
+effect_array = ['watercolor', 'blur', 'washedout', 'cartoon', 'hatch']
+i = 0; 
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
     print('running')
+    #camera.image_effect = 'colorswap'
     # Camera warm-up time
     time.sleep(2)
-    camera.capture('foo.jpg')
-    print('done!')
+    for x in range(5):
+        camera.image_effect = effect_array[i]
+        camera.capture('foo.jpg')
+        print('done!')
+        i+=1
+        time.sleep(0.1)
+
